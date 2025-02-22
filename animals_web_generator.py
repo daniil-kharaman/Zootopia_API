32e1fba24044eb44ee8d074c6c7b49514e867757
hard333777 <kharaman.daniil@gmail.com>
2025-02-22 19:36:03 +0100

    Added new functions and error handling

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,39 +1,66 @@
 from data_fetcher import fetch_data
 
 def serialize_animal(animal_obj):
-    '''Formats information about the animal fetched from the endpoint into html type'''
-    output = ''
-    output += '<li class="cards__item">'
-    output += f'<div class="card__title">{animal_obj['name'].capitalize()}</div>'
-    output += '<p class="card__text">'
-    if 'diet' in animal_obj['characteristics']:
-        output += f'<strong>Diet:</strong> {animal_obj['characteristics']['diet'].capitalize()}<br/>\n'
-    if 'locations' in animal_obj:
-        output += f'<strong>Location:</strong> {', '.join(animal_obj['locations']).title()}<br/>\n'
-    if 'type' in animal_obj['characteristics']:
-        output += f'<strong>Type:</strong> {animal_obj['characteristics']['type'].capitalize()}<br/>\n'
-    output += '</p>'
-    output += '</li>'
-    return output
+
+    """Formats information about the animal fetched from the endpoint into html type"""
+
+    #added try except
+    try:
+        output = ''
+        output += '<li class="cards__item">'
+        output += f'<div class="card__title">{animal_obj["name"].capitalize()}</div>'
+        output += '<p class="card__text">'
+        if 'diet' in animal_obj['characteristics']:
+            output += f'<strong>Diet:</strong> {animal_obj["characteristics"]["diet"].capitalize()}<br/>\n'
+        if 'locations' in animal_obj:
+            output += f'<strong>Location:</strong> {', '.join(animal_obj["locations"]).title()}<br/>\n'
+        if 'type' in animal_obj["characteristics"]:
+            output += f'<strong>Type:</strong> {animal_obj["characteristics"]["type"].capitalize()}<br/>\n'
+        output += '</p>'
+        output += '</li>'
+        return output
+    except KeyError:
+        print('Impossible to serialize.')
+
+#new functions
+def get_data_for_the_formatting(animals):
+    result = ''
+    if len(animals) == 0:
+        return result
+    for animal in animals:
+        # added the check
+        try:
+            result += serialize_animal(animal)
+        except TypeError:
+            print(f"{animal} has the wrong format and was not serialized")
+    return result
+
+
+def read_html_file(result, name):
+    with open('animals_template.html', 'r') as file:
+        data = file.read()
+    if len(result) == 0:
+        formatted_data = data.replace('__REPLACE_ANIMALS_INFO__',
+                                                f"<h1>The animal {name} doesn't exist</h1>")
+    else:
+        formatted_data = data.replace('__REPLACE_ANIMALS_INFO__', result)
+    return formatted_data
+
+
+def write_html_file(formatted_data):
+    with open('animals.html', 'w') as file:
+        file.write(formatted_data)
+    print('Website was successfully generated to the file animals.html.')
 
 
 def main():
     name = input('Enter a name of an animal: ').strip()
     animals = fetch_data(name)
-    result = ''
-    for animal in animals:
-        result += serialize_animal(animal)
-    with open('animals_template.html', 'r') as file:
-        data = file.read()
-    formatted_data = data.replace('__REPLACE_ANIMALS_INFO__', result)
-    if len(animals) == 0:
-        '<h1>My Animal Repository</h1>'
-        formatted_data = formatted_data.replace('<h1>My Animal Repository</h1>',
-                                                f"<h1>The animal {name} doesn't exist</h1>")
-    with open('animals.html', 'w') as file:
-        file.write(formatted_data)
-    print('Website was successfully generated to the file animals.html.')
-
+    if animals is None:
+        return
+    result = get_data_for_the_formatting(animals)
+    formatted_data = read_html_file(result, name)
+    write_html_file(formatted_data)
 
 if __name__ == '__main__':
     main()
--- data_fetcher.py
+++ data_fetcher.py
@@ -11,25 +11,24 @@ def fetch_data(animal_name):
   """
   Fetches the animals data for the animal 'animal_name'.
   Returns: a list of animals, each animal is a dictionary:
-  {
-    'name': ...,
-    'taxonomy': {
-      ...
-    },
-    'locations': [
-      ...
-    ],
-    'characteristics': {
-      ...
-    }
-  },
   """
+
   params = {
       'name': animal_name
   }
   headers = {
       'X-Api-Key': API_KEY
   }
-  response = requests.get(URL, params=params, headers=headers)
-  parsed_response = response.json()
-  return parsed_response
+  #added try except
+  try:
+      response = requests.get(URL, params=params, headers=headers)
+      parsed_response = response.json()
+      if parsed_response == {'error': 'Missing API Key.'}:
+          raise Exception('Missing API Key')
+      if parsed_response == {'error': 'name parameter must be provided.'}:
+          raise Exception('Missing the parameter "name" in the API request')
+      return parsed_response
+  except requests.exceptions.MissingSchema:
+      print('Impossible to reach the endpoint')
+  except Exception as e:
+      print(f"Such error occurred: {e}")
